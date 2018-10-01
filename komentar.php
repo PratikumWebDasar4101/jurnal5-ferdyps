ff3785b8388464f48e5ba6e33a8501659b4e0cb1
ferdyps <42725335+ferdyps@users.noreply.github.com>
2018-10-01 10:23:47 +0700

    Update komentar.php

--- komentar.php
+++ komentar.php
@@ -1,7 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Komentar</title>
+</head>
+<body>
+    <form method="post">
+        Komentar : <br>
+        <textarea name="komentar" cols="30" rows="10"></textarea><br><br>
+        <input type="submit" value="submit" name="submit">
+    </form>
+</body>
+</html>
 <?php
-//untuk yg komentar kalian bebas mau menggunakan 2 file atau satu file php
+    if(isset($_POST['submit'])){
+        $komentar=$_POST['komentar'];
 
-
-
-
+        $hitung = strlen($komentar);
+        if ($hitung < 5) {
+            echo "jumlah komentar kurang dari 5 kata";
+        }
+        else{
+            echo "jumlah komentar : $hitung";
+        }
+    }
 ?>
