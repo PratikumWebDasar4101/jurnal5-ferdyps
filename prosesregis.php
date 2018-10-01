3b9420167db7395fc2e180172138de94018ede8f
ferdyps <42725335+ferdyps@users.noreply.github.com>
2018-10-01 10:22:59 +0700

    Update prosesregis.php

--- prosesregis.php
+++ prosesregis.php
@@ -1,7 +1,10 @@
 <?php
+  $nim = $_POST['nim'];
+    $nama = $_POST['nama'];
+    $email = $_POST['email'];
 
-
-
-
-
+    echo "NIM : $nim <br>";
+    echo "Nama : $nama <br>";
+    echo "Email : $email <br><br>";
 ?>
+<a href="registrasi.php">LOGOUT</a>
