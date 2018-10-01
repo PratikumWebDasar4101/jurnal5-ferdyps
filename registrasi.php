194461246875c87ae9446e2c70ae2b7adc7e91d0
ferdyps <42725335+ferdyps@users.noreply.github.com>
2018-10-01 10:22:09 +0700

    Update registrasi.php

--- registrasi.php
+++ registrasi.php
@@ -4,8 +4,11 @@
 	<title>Form Registrasi</title>
 </head>
 <body>
-
-
-
+ <form action="prosesregis.php" method="post">
+    NIM : <input type="text" name="nim" pattern="\d*" maxlength="10" required><br><br>
+    Nama : <input type="text" name="nama" maxlength="25" required><br><br>
+    Email : <input type="email" name="email" pattern="[a-z0-9._-]+@+[a-z]+.com"><br><br>
+    <input type="submit" value="submit">
+    </form>
 </body>
 </html>
